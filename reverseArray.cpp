95a192ec6365598440b9c611809151253d605d3e
Divyam Gupta <divyamgupta3000@gmail.com>
2023-10-31 17:47:01 +0530

    Create reverseArray.cpp
    Please accept my pull request

---
+++ reverseArray.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+void reverseArray(int arr[], int size) {
+    int start = 0;
+    int end = size - 1;
+
+    while (start < end) {
+        // Swap elements at start and end indices
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    reverseArray(arr, size);
+
+    std::cout << "\nReversed array: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
